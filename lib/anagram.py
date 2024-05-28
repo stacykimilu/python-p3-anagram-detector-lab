de19028508e3a7fd8f31082d9bb6749c5506ac24
Stacy <stacykimilu@gmail.com>
2024-05-28 16:34:16 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+# your code goes here!# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        return [word for word in word_list if sorted(word) == sorted_word]
